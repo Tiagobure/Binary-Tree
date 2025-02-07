6fadb36fe2db6beb5c5289f608514e011371cb74
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-02-07 19:23:24 -0300

    Fix: Element<TIPO>

--- src/binaryTree/Element.java
+++ src/binaryTree/Element.java
@@ -1,6 +1,39 @@
 package binaryTree;
 
 public class Element<TIPO> {
+	private TIPO value;
+	private Element<TIPO> left;
+	private Element<TIPO> right;
 
+	public Element(TIPO value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+
+	public TIPO getValue() {
+		return value;
+	}
+
+	public void setValue(TIPO value) {
+		this.value = value;
+	}
+
+	public Element<TIPO> getLeft() {
+		return left;
+	}
+
+	public void setLeft(Element<TIPO> left) {
+		this.left = left;
+	}
+
+	public Element<TIPO> getRight() {
+		return right;
+	}
+
+	public void setRight(Element<TIPO> right) {
+		this.right = right;
+	}
 	
+
 }

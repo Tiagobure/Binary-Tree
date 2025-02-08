f6c03ff0076203b5dc4d906b9d8d2df2c1a8b6cb
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-02-07 21:01:32 -0300

    Fix: tree Order

--- src/binaryTree/Main.java
+++ src/binaryTree/Main.java
@@ -6,8 +6,23 @@ public class Main {
 
 		Tree<Integer> tree = new Tree<Integer>();
 		tree.toAdd(10);
-		tree.toAdd(4);
+		tree.toAdd(8);
+		tree.toAdd(5);
+		tree.toAdd(9);
+		tree.toAdd(7);
+		tree.toAdd(18);
+		tree.toAdd(13);
+		tree.toAdd(20);
+		
+		System.out.println("pre order:");
+		tree.preOrder(tree.getRoot());
 
+		System.out.println("\nin order:");
+		tree.inOrder(tree.getRoot());
+		
+				
+		System.out.println("\npos order:");
+		tree.posOrder(tree.getRoot());
 	}
 
 }
--- src/binaryTree/Tree.java
+++ src/binaryTree/Tree.java
@@ -11,29 +11,59 @@ public class Tree<TIPO extends Comparable> {
 	public void toAdd(TIPO value) {
 		Element<TIPO> newElement = new Element<TIPO>(value);
 
-		if(root == null) {
+		if (root == null) {
 			this.root = newElement;
-		}else {
+		} else {
 			Element<TIPO> current = this.root;
-			 while(true) {
-				 if(newElement.getValue().compareTo(current.getValue()) == -1) {
-					 if(current.getLeft() != null) {
-						 current = current.getLeft();
-					 }else {
-						 current.setLeft(newElement);
-						 break;
-					 }
-				 }else {
-					 if (current.getRight() != null) {
-						 current = current.getRight();
-					 }else {
-						 current.setRight(newElement);
-						 break;
-					 }
-				 }
-			 }
-				 
-			 
+			while (true) {
+				if (newElement.getValue().compareTo(current.getValue()) == -1) {
+					if (current.getLeft() != null) {
+						current = current.getLeft();
+					} else {
+						current.setLeft(newElement);
+						break;
+					}
+				} else {
+					if (current.getRight() != null) {
+						current = current.getRight();
+					} else {
+						current.setRight(newElement);
+						break;
+					}
+				}
+			}
+
+		}
+
+	}
+	
+
+	public Element<TIPO> getRoot() {
+		return root;
+	}
+
+	public void preOrder(Element<TIPO> current) {
+		if (current != null) {
+			System.out.println(current.getValue());
+			preOrder(current.getLeft());
+			
+			preOrder(current.getRight());
+		}
+	}
+	public void inOrder(Element<TIPO> current) {
+		if (current != null) {
+			inOrder(current.getLeft());
+			System.out.println(current.getValue());
+			inOrder(current.getRight());
+		}
+	}
+	
+	public void posOrder(Element<TIPO> current) {
+		if (current != null) {
+			posOrder(current.getLeft());
+			posOrder(current.getRight());
+			System.out.println(current.getValue());
+			
 		}
 	}
 }
